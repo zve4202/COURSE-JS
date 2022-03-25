2dcac13676bee5e50ff96cf5bb73e2c25a7a2c08
Вячеслав <zve4202@yandex.ru>
2022-03-25 13:46:25 +0300

    first project

---
+++ first-project/index.js
@@ -0,0 +1,7 @@
+console.log('Запускаем таймер');
+
+for (let index = 5; index >= 0; index--) {
+    window.alert(index);
+}  
+
+console.log("Обратный отсчет закончен");
---
+++ first-project/js-info.js
@@ -0,0 +1,5 @@
+alert("Брендан Айк");
+alert("За 10 дней");
+alert("JavaScript появился в 1995 году");
+alert("JavaScript это не Java");
+alert("ECMAScript");
