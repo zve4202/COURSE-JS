9b951b26bd1bc8cccef0320d014507ebccc83f06
Вячеслав <zve4202@yandex.ru>
2022-03-26 18:43:02 +0300

    Lesson 2

---
+++ lesson_2/car.js
@@ -0,0 +1,18 @@
+const carName = 'Mersedes';
+let maxCarSpeed = 230;    
+let carOwner = 'Johan'; 
+
+window.alert(carName);
+window.alert(`${maxCarSpeed} kM/h`);
+window.alert(carOwner);
+
+maxCarSpeed = 200;
+console.log('У машины изменилась скорость', maxCarSpeed);
+carOwner = "Otto";
+console.log("У машины изменился владелец", carOwner);
+
+maxCarSpeed = 195;
+console.log("У машины изменилась скорость", maxCarSpeed);
+carOwner = "Max";
+console.log("У машины изменился владелец", carOwner);
+
---
+++ lesson_2/index.js
@@ -0,0 +1,16 @@
+// JavaScript код с созданием переменных
+/*
+Автор:
+Вячеслав Зубачев
+*/
+const myName = 'Вячеслав';
+const mySurname = 'Зубачев';
+let myFavoriteDrink = 'Вятский квас';
+let myFavoriteAnimal = 'Собака';
+let myFavoriteProgrammingLanguage = 'JavaScript, C#';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
